a6d68af09ed82b81ca933bf967d0f9fd1db78b14
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-07 16:07:27 +1100

    Add js files

---
+++ getters-and-setters.js
@@ -0,0 +1,24 @@
+ class Square {
+     constructor(_width) {
+         this.width = _width;
+         this.height = _width;
+         this.numberOfRequestsForArea = 0;
+     }
+     get area() {
+         this.numberOfRequestsForArea++;
+         return this.width * this.height;
+     }
+     set area(area) {
+         this.width = Math.sqrt(area);
+         this.height = this.width;
+     }
+
+ }
+ let square1 = new Square(25);
+ square1.area = 25;
+
+ console.log(square1.area);
+ console.log(square1.area);
+ console.log(square1.area);
+ console.log(square1.area);
+ console.log(square1.numberOfRequestsForArea);
---
+++ statick-methods.js
@@ -0,0 +1,17 @@
+class Square {
+    constructor(_width) {
+        this.width = _width;
+        this.height = _width;
+    }
+    //a and b - are 2 different square instances
+    static equals(a, b) {
+        return a.width * a.height === b.width * b.height;
+    }
+    static isValidDimensions(width, height) {
+        return width == height;
+    }
+}
+
+let square1 = new Square(9);
+let square2 = new Square(9);
+console.log(Square.isValidDimensions(5, 5));
